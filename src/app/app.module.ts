54541c3f86fb3262371cf7e9642920f7e52139e6
Rajuri <snehithudu.com@gmail.com>
2023-09-08 13:14:42 +0530

    nav c created

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,13 +6,15 @@ import { AppComponent } from './app.component';
 import { HeaderComponent } from './header/header.component';
 import { FooterComponent } from './footer/footer.component';
 import { CategoriesComponent } from './categories/categories.component';
+import { NavComponent } from './nav/nav.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HeaderComponent,
     FooterComponent,
-    CategoriesComponent
+    CategoriesComponent,
+    NavComponent
   ],
   imports: [
     BrowserModule,
